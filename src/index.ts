25340a68881bd5ca9008292f4c918d6f224a3efd
Brycen Dorsay <dorsay@live.ca>
2021-05-24 17:38:20 -0700

    redone in typescript to match slack-action

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { setOutput, setFailed } from '@actions/core';
+import github from '@actions/github';
+
+export default function main() {
+  const pr = github.context.payload.pull_request;
+  if (!pr) throw 'Not a pull request';
+
+  // additions
+  setOutput('additions', pr.additions);
+  // deletions
+  setOutput('deletions', pr.deletions);
+  // changed_files
+  setOutput('changed_files', pr.changed_files);
+  // commits
+  setOutput('commits', pr.commits);
+  // assignee
+  setOutput('assignee', pr.assignee);
+  // number
+  setOutput('number', pr.number);
+  // title
+  setOutput('title', pr.title);
+  // body
+  setOutput('body', pr.body);
+  // created_at
+  setOutput('created_at', pr.created_at);
+  // updated_at
+  setOutput('updated_at', pr.updated_at);
+  // url
+  setOutput('url', pr.html_url);
+  // base_branch
+  setOutput('base_branch', pr.base.ref);
+  // base_commit
+  setOutput('base_commit', pr.base.sha);
+  // head_branch
+  setOutput('head_branch', pr.head.ref);
+  // head_commit
+  setOutput('head_commit', pr.head.sha);
+  // draft
+  setOutput('draft', pr.draft);
+}
+
+try {
+  main();
+} catch (err) {
+  setFailed(err.message);
+}
